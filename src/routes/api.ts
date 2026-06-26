4283c23fdac99cb81aacc0f92071bb8af0efd357
Jason Zheng <jasonzheng85@163.com>
2026-06-26 19:33:08 +0800

    try playwright

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,4 +1,5 @@
 import { GET, POST } from "../router";
+import { launch } from "@cloudflare/playwright";
 
 type OrderRow = {
   Id: string;
@@ -19,4 +20,32 @@ POST("/post", async (request) => {
 GET("/api/db", async (_,env) => {
 	const result = await env.DB.prepare("SELECT Id, CustomerName, OrderDate FROM [Order] LIMIT 100").run() as OrderRow[];
 	return Response.json({ message: "Hello World!", result: result });
+});
+
+GET("/api/browser", async (_,env) => {
+	const browser = await launch(env.MYBROWSER);
+	const page = await browser.newPage();
+
+    await page.goto("https://demo.playwright.dev/todomvc");
+
+    const TODO_ITEMS = [
+      "buy some cheese",
+      "feed the cat",
+      "book a doctors appointment",
+    ];
+
+    const newTodo = page.getByPlaceholder("What needs to be done?");
+    for (const item of TODO_ITEMS) {
+      await newTodo.fill(item);
+      await newTodo.press("Enter");
+    }
+
+    const img = await page.screenshot();
+    await browser.close();
+
+    return new Response(img, {
+      headers: {
+        "Content-Type": "image/png",
+      },
+    });
 });
